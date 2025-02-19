4667e22db4d467e2956902aed07de7270c0aafed
IRonik <131461245+RNKCD@users.noreply.github.com>
2025-02-19 00:02:00 -0600

    Add files via upload
    It is a class file that uses Comparable object.

--- MergeSort.java
+++ MergeSort.java
@@ -1,68 +1,86 @@
-import java.util.Arrays;
-public class MergeSort {
-    public static void main(String[] args) {
-        int[] arr = {1,4,3,6,7,8,5,2,4,7,};
-        System.out.println(Arrays.toString(arr));
-        
-        
-        mergesort(arr);
-        System.out.println(Arrays.toString(arr));
-        
-                
-    }
-        
-    private static void mergesort( int[] arr) {
-        int n = arr.length;
-        //return if there is 1 element
-        if(arr.length <= 1){
-            return;
-        }
-
-        int mid = n/2;
-
-        //creating a new left and right array that will re-loop
-        int[] left_arr = Arrays.copyOfRange(arr, 0, mid);
-        int[] right_arr = Arrays.copyOfRange(arr, mid, n);
-
-
-        //splitting the array to even smaller pieces
-        mergesort(left_arr);
-        mergesort(right_arr);
-        merge(arr, left_arr, right_arr);
-        
-    }
-
-    private static void merge(int[] arr, int[] left_arr, int[] right_arr){
-        int i=0, j=0, k =0;
-        //will compare the values of left and right array till one array is empty
-        //then the remaining number in array is already sorted 
-        //we can just add it onto the big array
-        while(i < left_arr.length && j < right_arr.length){
-            if(left_arr[i] < right_arr[j]){
-                arr[k] = left_arr[i];
-                i++;
-                k++;
-            }
-            else{
-                arr[k] = right_arr[j];
-                j++;
-                k++;
-            }
-        }
-
-        //will fill the remaining spot of arr if sorted left has more element to be set
-        while(i < left_arr.length){
-            arr[k] = left_arr[i];
-                i++;
-                k++;
-        }
-
-        //will fill the remaining spot of arr if sorted right has more element to be set
-        while(j < right_arr.length){
-            arr[k] = right_arr[j];
-                j++;
-                k++;
-        }
-        
-    }
+class MergeSort<T extends Comparable<T>>
+{
+   private T[] list;
+   
+   public MergeSort(T[] l)
+   {
+      list = l;
+   }
+   
+   public String toString()
+   {
+      //System.out.println(list.length);
+      String rs = "";
+      for(T item : list)
+      {
+         rs += item + ", ";
+      }
+      rs = rs.substring(0, rs.length()-2);
+      return rs;
+   }
+   
+   public void sort()
+   {
+      mergesort(list.length, list);
+   }
+   
+   public void mergesort(int n, T[] S)
+   {
+      if(n > 1)
+      {
+         int h = n/2;
+         int m = n - h;
+         T[] u = (T[]) new Comparable[h];
+         T[] v = (T[]) new Comparable[m];
+         copy(0, h, S, u);
+         copy(h, m, S, v);
+         mergesort(h, u);
+         mergesort(m, v);
+         merge(h, m, u, v, S);
+      }
+   }
+   
+   public void copy(int s, int n, T[] a, T[] b)
+   {
+      int k=0;
+      for(int i = s; i < n+s; i++)
+      {
+         b[k] = a[i];
+         k++;
+      }
+   }
+   
+   public void merge(int h, int m, T[] u, T[] v, T[] S)
+   {
+      int i=0, j=0, k=0;
+      
+      while(i<h && j<m)
+      {
+         if(((T)u[i]).compareTo(v[j]) < 0)
+         {
+            S[k] = u[i];
+            i++;
+         } else 
+         {
+            S[k] = v[j];
+            j++;
+         }
+         k++;
+       }
+       if(i>=h)
+       {
+         for(int p = j; p<m; p++)
+         {
+            S[k] = v[p];
+            k++;
+         }
+       } else 
+       {
+         for(int p = i; p<h; p++)
+         {
+            S[k] = u[p];
+            k++;
+         }
+       }
+   }
 }
